f85bada43364691acd7dcfa467d4e39c0af2bd46
Mika van Os <os0017@hz.nl>
2025-06-02 10:52:59 +0200

    Setting up initial Dockerfiles for project

--- app/Http/Controllers/StaticContentController.php
+++ app/Http/Controllers/StaticContentController.php
@@ -6,16 +6,19 @@ use Illuminate\Http\Request;
 
 class StaticContentController extends Controller
 {
+//    Returns the HomePage for the Project
     public function homePage()
     {
         return view('home');
     }
 
+//    Returnst the About Page for the Project
     public function aboutMe()
     {
         return view('about_me');
     }
 
+//    Returns the Grades-dashboard for the project
     public function dashboard()
     {
         return view('dashboard');
