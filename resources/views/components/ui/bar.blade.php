ff4536cc4b757c2b2633579087e196411239b7d0
Mika van Os <os0017@hz.nl>
2025-06-11 10:56:03 +0200

    Implementing forced authentication in order to access the future dashboard

--- resources/views/components/ui/bar.blade.php
+++ resources/views/components/ui/bar.blade.php
@@ -1,4 +1,6 @@
 <nav>
     <x-ui.nav :request="$request" route="welcome">Home page</x-ui.nav>
-    <x-ui.nav :request="$request" route="dashboard">Dashboard</x-ui.nav>
+    @auth
+        <x-ui.nav :request="$request" route="dashboard">Dashboard</x-ui.nav>
+    @endauth
 </nav>
