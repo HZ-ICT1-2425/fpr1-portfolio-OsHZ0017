c9303cd4ecf1aa38b91291684dfa68b0d354c42c
Mika van Os <os0017@hz.nl>
2025-06-11 10:27:39 +0200

    Builiding the tables for my future dashboard implementation

---
+++ app/Http/Controllers/SchoolyearController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Schoolyear;
+use Illuminate\Http\Request;
+
+class SchoolyearController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Schoolyear $schoolyear)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Schoolyear $schoolyear)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Schoolyear $schoolyear)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Schoolyear $schoolyear)
+    {
+        //
+    }
+}
